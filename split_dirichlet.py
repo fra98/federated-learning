9ab5c3b33748920087f5e19e7b0ce61c50c1d225
Francesco Torta <62566275+Fra98@users.noreply.github.com>
2022-01-15 20:23:34 +0100

    Non-IID splitter improved

--- split_dirichlet.py
+++ split_dirichlet.py
@@ -1,40 +1,37 @@
 from copy import deepcopy
 import numpy as np
+import sys
+import random
 
 from src.utils import load_cifar
 
 NUM_CLASSES = 10
 NUM_CLIENTS = 100
 ALPHA = 1
+CB = True
 
 trainset = load_cifar(name=f'CIFAR{NUM_CLASSES}', train=True)
 classes_IDs = [i for i in range(NUM_CLASSES)]
 client_data_size = len(trainset) // NUM_CLIENTS
 num_samples_per_class = len(trainset) // NUM_CLASSES
 
+# Indexes sorted by class label 
+sorted_indexes = np.argsort(trainset.targets)
 
-'''
-def main_dirichlet_WRONG():
-    trainset = load_cifar(name='CIFAR10', train=True)
-    clients = [i for i in range(NUM_CLIENTS)]
-    map = np.zeros((NUM_CLIENTS, NUM_CLASSES), dtype=np.int32)
 
-    priors = np.ones(NUM_CLIENTS) # * (1/NUM_CLIENTS) ?
-    probabilities = np.random.dirichlet(ALPHA * priors, NUM_CLASSES)
-    # print("Probabilities matrix -> Row: CLASS, Col: USERS")
-    # print(probabilities)
-
-    for idx in range(len(trainset)):
-        class_label = trainset[idx][1]
-        user_id = np.random.choice(clients, p=probabilities[class_label])
-        map[user_id][class_label] += 1
-
+def print_stats(map):
+    np.set_printoptions(precision=2, suppress=True)
+    print(f"ALPHA = {ALPHA}, NUM_CLIENTS = {NUM_CLIENTS}")
     print("Class counter per user -> Row: USERS, Col: CLASS")
     print(map)
-
     print("Size dataset per user")
     print(map.sum(axis=1))
-'''
+    print("Size dataset per class")
+    print(map.sum(axis=0))
+    print("Total size")
+    print(map.sum())
+    print("Percentage of classes for each user")
+    print(100 * map / map.sum(axis=1).reshape(NUM_CLIENTS, 1))
 
 
 def dirichlet_probabilities(num_classes, num_clients, alpha):
@@ -43,44 +40,12 @@ def dirichlet_probabilities(num_classes, num_clients, alpha):
     return probabilities
 
 
-def print_stats(map):
-    print("Class counter per user -> Row: USERS, Col: CLASS")
-    print(map)
-    print("Size dataset per user")
-    print(map.sum(axis=1))
-    print("Size dataset per class")
-    print(map.sum(axis=0))
-
-
-def main_dirichlet():
-    map = np.zeros((NUM_CLIENTS, NUM_CLASSES), dtype=np.int32)
-
-    probabilities = dirichlet_probabilities(NUM_CLASSES, NUM_CLIENTS, ALPHA)
-    print("Probabilities matrix -> Row: USERS, Col: CLASS")
-    np.set_printoptions(precision=2, suppress=True)
-    print(probabilities)
-    
-    for user_id in range(NUM_CLIENTS):
-        for _ in range(client_data_size):
-            class_label = np.random.choice(classes_IDs, p=probabilities[user_id])
-            map[user_id][class_label] += 1
-
-    print_stats(map)
-
-
 def main_cifar_split():
     map = np.zeros((NUM_CLIENTS, NUM_CLASSES), dtype=np.int32)
-    
-    # Create 2D array of [image_id, class] SORTED by classes 
-    indexer = np.empty((len(trainset), 2), dtype=np.int32)  # image_id, class
-    for i in range(len(trainset)):
-        indexer[i][0] = i
-        indexer[i][1] = trainset[i][1]
-    indexer = indexer[np.argsort(indexer[:, 1])]
 
     # Dirichlet probability distribution -> 2D matrix, shape: (NUM_CLIENTS, NUM_CLASSES)
     dirichlet_probs = dirichlet_probabilities(NUM_CLASSES, NUM_CLIENTS, ALPHA)
-
+    
     offset_class = np.zeros(NUM_CLASSES, dtype=np.int32)
     indexes = []
     for user_id in range(NUM_CLIENTS):
@@ -93,7 +58,7 @@ def main_cifar_split():
                 offset_class[class_label] = 0
             pos = num_samples_per_class * class_label + offset_class[class_label]  # base + offset
 
-            image_id = indexer[pos][0]
+            image_id = sorted_indexes[pos]
             user_indexes.append(image_id)
             offset_class[class_label] += 1
 
@@ -102,7 +67,44 @@ def main_cifar_split():
     print_stats(map)
     
 
+def main_cifar_split_class_balanced():
+    map = np.zeros((NUM_CLIENTS, NUM_CLASSES), dtype=np.int32)
+
+    # Dirichlet probability distribution -> 2D matrix, shape: (NUM_CLIENTS, NUM_CLASSES)
+    dirichlet_probs = dirichlet_probabilities(NUM_CLASSES, NUM_CLIENTS, ALPHA)
+
+    offset_class = np.zeros(NUM_CLASSES, dtype=np.int32)
+    users_indexes = []
+    for _ in range(NUM_CLIENTS):
+        users_indexes.append([])
+
+    shuffled_classes = deepcopy(classes_IDs)
+    for user_id in range(NUM_CLIENTS):      
+        for _ in range(client_data_size):     
+            class_label = np.random.choice(classes_IDs, p=dirichlet_probs[user_id])
+
+            if offset_class[class_label] >= num_samples_per_class:
+                random.shuffle(shuffled_classes)
+                for c in shuffled_classes:
+                    if offset_class[c] < num_samples_per_class:
+                        class_label = c
+
+            pos = num_samples_per_class * class_label + offset_class[class_label]  # base + offset
+            image_id = sorted_indexes[pos]
+            users_indexes[user_id].append(image_id)
+
+            offset_class[class_label] += 1
+            map[user_id][class_label] += 1
+
+    print_stats(map)
+
 
 if __name__ == '__main__':
-    # main_dirichlet()
-    main_cifar_split()
+    if len(sys.argv) == 3:
+        ALPHA = float(sys.argv[1])
+        CB = bool(int((sys.argv[2])))
+
+    if CB:
+        main_cifar_split_class_balanced()
+    else :
+        main_cifar_split()
--- split_google.py
+++ split_google.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import numpy as np
 
@@ -5,15 +6,21 @@ NUM_CLASSES = 10
 ALPHA = 1
 
 def print_stats(map):
+    np.set_printoptions(precision=2, suppress=True)
+    print(f"ALPHA = {ALPHA}, NUM_CLIENTS = 100")
     print("Class counter per user -> Row: USERS, Col: CLASS")
     print(map)
     print("Size dataset per user")
     print(map.sum(axis=1))
     print("Size dataset per class")
     print(map.sum(axis=0))
+    print("Total size")
+    print(map.sum())
+    print("Percentage of classes for each user")
+    print(100 * map / map.sum(axis=1).reshape(100, 1))
 
 
-if __name__ == "__main__":
+def main():
     map = np.zeros((100, NUM_CLASSES), dtype=np.int64)
 
     with open(f"./CIFAR/CIFAR{NUM_CLASSES}_splits/federated_train_alpha_{ALPHA:.2f}.csv", "r") as f:
@@ -24,4 +31,11 @@ if __name__ == "__main__":
             map[int(user_id)][int(class_label)] += 1
 
     print_stats(map)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        ALPHA = float(sys.argv[1])
+
+    main()
 
