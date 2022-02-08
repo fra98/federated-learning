b081d38a902fe6405cb30b232061bf0647a870fb
Francesco Torta <62566275+Fra98@users.noreply.github.com>
2022-02-08 15:52:49 +0100

    New log parser

--- log_parser.py
+++ log_parser.py
@@ -3,22 +3,10 @@ import matplotlib.pyplot as plt
 
 BASE_PATH = "logs/"
 
-LOGS_TRAIN = ["train_acc_01.txt", "train_acc_02.txt", "train_acc_03.txt",
-              "train_acc_04.txt", "train_acc_05.txt", "train_acc_06.txt",
-              "train_acc_07.txt", "train_acc_08.txt", "train_acc_09.txt",
-              "train_acc_10.txt", "train_acc_11.txt", "train_acc_12.txt",
-              "train_acc_13.txt", "train_acc_14.txt", "train_acc_15.txt",
-              "train_acc_16.txt", "train_acc_17.txt", "train_acc_18.txt",
-              "train_acc_19.txt"]
-
-LOGS_TEST = ["test_acc_01.txt", "test_acc_02.txt", "test_acc_03.txt",
-             "test_acc_04.txt", "test_acc_05.txt", "test_acc_06.txt",
-             "test_acc_07.txt", "test_acc_08.txt", "test_acc_09.txt",
-             "test_acc_10.txt", "test_acc_11.txt", "test_acc_12.txt",
-             "test_acc_13.txt", "test_acc_14.txt", "test_acc_15.txt",
-             "test_acc_16.txt", "test_acc_17.txt", "test_acc_18.txt",
-             "test_acc_19.txt"]
-
+LOGS = [     1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 
+                22, 23
+        ]
 
 vect_train_rounds = []
 vect_train_acc = []
@@ -53,70 +41,43 @@ def get_results(path):
 
 
 if __name__ == '__main__':
-    for log in LOGS_TRAIN:
-        round, train_acc, train_loss = get_results(BASE_PATH + log)
-        vect_train_rounds.append(round)
-        vect_train_acc.append(train_acc)
-        vect_train_loss.append(train_loss)
-        
-    for log in LOGS_TEST:
-        round, test_acc, test_loss = get_results(BASE_PATH + log)    
-        vect_test_rounds.append(round)
-        vect_test_acc.append(test_acc)
-        vect_test_loss.append(test_loss)
+    for config in range(70):
+        if config in LOGS:
+            round, train_acc, train_loss = get_results(BASE_PATH + f"train_acc_{config:02}.txt")
+            vect_train_rounds.append(round)
+            vect_train_acc.append(train_acc)
+            vect_train_loss.append(train_loss)
+            
+            round, test_acc, test_loss = get_results(BASE_PATH + f"test_acc_{config:02}.txt")    
+            vect_test_rounds.append(round)
+            vect_test_acc.append(test_acc)
+            vect_test_loss.append(test_loss)
+        else:
+            vect_train_rounds.append([])
+            vect_train_acc.append([])
+            vect_train_loss.append([])
+            vect_test_rounds.append([])
+            vect_test_acc.append([])
+            vect_test_loss.append([])
 
 
-    for i in range(len(vect_test_acc)):
+    for i in range(70):
         assert len(vect_train_rounds[i]) == len(vect_train_acc[i])
         assert len(vect_test_rounds[i]) == len(vect_test_acc[i])
         assert len(vect_test_acc[i]) == len(vect_train_acc[i])
 
+
+    # PLOTTING
+    
     plt.figure()
     plt.xlabel("Rounds")
     plt.ylabel("Accuracy")
     
-    # Train
-    # plt.plot(vect_train_rounds[0], vect_train_acc[0], label='train 1')
-    # plt.plot(vect_train_rounds[1], vect_train_acc[1], label='train 2')
-    # plt.plot(vect_train_rounds[2], vect_train_acc[2], label='train 3')
-    # plt.plot(vect_train_rounds[3], vect_train_acc[3], label='train 4')
-    # plt.plot(vect_train_rounds[4], vect_train_acc[4], label='train 5')
-    # plt.plot(vect_train_rounds[5], vect_train_acc[5], label='train 6')
-    # plt.plot(vect_train_rounds[6], vect_train_acc[6], label='train 7')
-    # plt.plot(vect_train_rounds[7], vect_train_acc[7], label='train 8')
-    # plt.plot(vect_train_rounds[8], vect_train_acc[8], label='train 9') 
-    # plt.plot(vect_train_rounds[9], vect_train_acc[9], label='train 10') 
-    # plt.plot(vect_train_rounds[10], vect_train_acc[10], label='train 11') 
-    # plt.plot(vect_train_rounds[11], vect_train_acc[11], label='train 12') 
-    # plt.plot(vect_train_rounds[12], vect_train_acc[12], label='train 13') 
-    # plt.plot(vect_train_rounds[13], vect_train_acc[13], label='train 14') 
-    # plt.plot(vect_train_rounds[14], vect_train_acc[14], label='train 15') 
-    # plt.plot(vect_train_rounds[15], vect_train_acc[15], label='train 16') 
-    # plt.plot(vect_train_rounds[16], vect_train_acc[16], label='train 17') 
-    # plt.plot(vect_train_rounds[17], vect_train_acc[17], label='train 18') 
-    # plt.plot(vect_train_rounds[18], vect_train_acc[18], label='train 19') 
+    SHOW = [18, 19]
 
-    
-    # Test
-    plt.plot(vect_test_rounds[0], vect_test_acc[0], label='test 1')
-    plt.plot(vect_test_rounds[1], vect_test_acc[1], label='test 2')
-    plt.plot(vect_test_rounds[2], vect_test_acc[2], label='test 3')
-    plt.plot(vect_test_rounds[3], vect_test_acc[3], label='test 4')
-    plt.plot(vect_test_rounds[4], vect_test_acc[4], label='test 5')
-    plt.plot(vect_test_rounds[5], vect_test_acc[5], label='test 6')
-    plt.plot(vect_test_rounds[6], vect_test_acc[6], label='test 7')
-    plt.plot(vect_test_rounds[7], vect_test_acc[7], label='test 8')
-    plt.plot(vect_test_rounds[8], vect_test_acc[8], label='test 9')
-    plt.plot(vect_test_rounds[9], vect_test_acc[9], label='test 10') 
-    plt.plot(vect_test_rounds[10], vect_test_acc[10], label='test 11') 
-    plt.plot(vect_test_rounds[11], vect_test_acc[11], label='test 12') 
-    plt.plot(vect_test_rounds[12], vect_test_acc[12], label='test 13') 
-    plt.plot(vect_test_rounds[13], vect_test_acc[13], label='test 14') 
-    plt.plot(vect_test_rounds[14], vect_test_acc[14], label='test 15') 
-    plt.plot(vect_test_rounds[15], vect_test_acc[15], label='test 16') 
-    plt.plot(vect_test_rounds[16], vect_test_acc[16], label='test 17') 
-    plt.plot(vect_test_rounds[17], vect_test_acc[17], label='test 18') 
-    plt.plot(vect_test_rounds[18], vect_test_acc[18], label='test 19') 
+    for i in SHOW:
+        plt.plot(vect_train_rounds[i], vect_train_acc[i], label=f'train {i}')
+        plt.plot(vect_test_rounds[i], vect_test_acc[i], label=f'test {i}')
     
     plt.legend(loc='lower right')
     plt.show()
--- log_parser_OLD.py
+++ log_parser_OLD.py
@@ -3,138 +3,121 @@ import matplotlib.pyplot as plt
 
 BASE_PATH = "logs/"
 
-LOGS = ["config_01.log", "config_02.log", "config_03.log",
-        "config_04.log", "config_05.log", "config_06.log",
-        "config_07.log", "config_08.log", "config_09.log",
-        "config_10.log", "config_11.log", "config_12.log",
-        "config_13.log", "config_14.log", "config_15.log",
-        "config_16.log", "config_17.log", "config_18.log"]
+LOGS_TRAIN = ["train_acc_01.txt", "train_acc_02.txt", "train_acc_03.txt",
+              "train_acc_04.txt", "train_acc_05.txt", "train_acc_06.txt",
+              "train_acc_07.txt", "train_acc_08.txt", "train_acc_09.txt",
+              "train_acc_10.txt", "train_acc_11.txt", "train_acc_12.txt",
+              "train_acc_13.txt", "train_acc_14.txt", "train_acc_15.txt",
+              "train_acc_16.txt", "train_acc_17.txt", "train_acc_18.txt",
+              "train_acc_19.txt"]
 
+LOGS_TEST = ["test_acc_01.txt", "test_acc_02.txt", "test_acc_03.txt",
+             "test_acc_04.txt", "test_acc_05.txt", "test_acc_06.txt",
+             "test_acc_07.txt", "test_acc_08.txt", "test_acc_09.txt",
+             "test_acc_10.txt", "test_acc_11.txt", "test_acc_12.txt",
+             "test_acc_13.txt", "test_acc_14.txt", "test_acc_15.txt",
+             "test_acc_16.txt", "test_acc_17.txt", "test_acc_18.txt",
+             "test_acc_19.txt"]
+
+
+vect_train_rounds = []
 vect_train_acc = []
 vect_train_loss = []
+
+vect_test_rounds = []
 vect_test_acc = []
 vect_test_loss = []
 
+
 def get_results(path):
-    train_acc = np.zeros(1000)
-    train_loss = np.zeros(1000)
-    test_acc = np.zeros(1000)
-    test_loss = np.zeros(1000)
-    curr_round = None
+    NULL = -42
+    vet_rounds = np.ones(1000) * NULL
+    vet_acc = np.ones(1000) * NULL
+    vet_loss = np.ones(1000) * NULL
 
     with open(path, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            if "ROUND" in line:
-                curr_round = int(line.split(" ")[4])
+            round = int(line.split(" ")[0])
+            accuracy = float(line.split(" ")[1])
+            loss = float(line.split(" ")[2])
+            vet_rounds[round] = round
+            vet_acc[round] = accuracy
+            vet_loss[round] = loss
 
-            elif "Server -> Train" in line:
-                strings = line.split(" ")
-                loss = float(strings[7])
-                accuracy = float(strings[11])
-                train_loss[curr_round-1] = loss
-                train_acc[curr_round-1] = accuracy
+    vet_rounds = vet_rounds[vet_rounds != NULL]
+    vet_acc = vet_acc[vet_acc != NULL]
+    vet_loss = vet_loss[vet_loss != NULL]
 
-            elif "Server -> Test" in line:
-                strings = line.split(" ")
-                loss = float(strings[7])
-                accuracy = float(strings[11])
-                test_loss[curr_round-1] = loss
-                test_acc[curr_round-1] = accuracy
-
-    return train_acc, train_loss, test_acc, test_loss
+    return vet_rounds, vet_acc, vet_loss
 
 
 if __name__ == '__main__':
-    for log in LOGS:
-        train_acc, train_loss, test_acc, test_loss = get_results(BASE_PATH + log)
+    for log in LOGS_TRAIN:
+        round, train_acc, train_loss = get_results(BASE_PATH + log)
+        vect_train_rounds.append(round)
         vect_train_acc.append(train_acc)
         vect_train_loss.append(train_loss)
+        
+    for log in LOGS_TEST:
+        round, test_acc, test_loss = get_results(BASE_PATH + log)    
+        vect_test_rounds.append(round)
         vect_test_acc.append(test_acc)
         vect_test_loss.append(test_loss)
 
-    vet_rounds = []
-    
-    STEP = 15
-    for i in range(9):
-        v = vect_train_acc[i]
-        v = v[v > 0]
-        v = v[STEP-1::STEP]
-        vect_train_acc[i] = v
-
-        s = vect_test_acc[i]
-        s = s[s > 0]
-        vect_test_acc[i] = s
-
-        rounds = np.arange(1, len(vect_train_acc[i])+1) * STEP
-        vet_rounds.append(rounds)
-
-    
-    STEP = 5
-    for i in range(9, 19):
-        v = vect_train_acc[i]
-        v = v[v > 0]
-        vect_train_acc[i] = v
-
-        s = vect_test_acc[i]
-        s = s[s > 0]
-        vect_test_acc[i] = s
-
-        rounds = np.arange(1, len(vect_train_acc[i])+1) * STEP
-        vet_rounds.append(rounds)
-
 
     for i in range(len(vect_test_acc)):
+        assert len(vect_train_rounds[i]) == len(vect_train_acc[i])
+        assert len(vect_test_rounds[i]) == len(vect_test_acc[i])
         assert len(vect_test_acc[i]) == len(vect_train_acc[i])
 
-
     plt.figure()
-    plt.legend()
     plt.xlabel("Rounds")
     plt.ylabel("Accuracy")
     
     # Train
-    plt.plot(vet_rounds[0], vect_train_acc[0], label='train conf 1')
-    plt.plot(vet_rounds[1], vect_train_acc[1], label='train conf 2')
-    plt.plot(vet_rounds[2], vect_train_acc[2], label='train conf 3')
-    plt.plot(vet_rounds[3], vect_train_acc[3], label='train conf 4')
-    plt.plot(vet_rounds[4], vect_train_acc[4], label='train conf 5')
-    plt.plot(vet_rounds[5], vect_train_acc[5], label='train conf 6')
-    plt.plot(vet_rounds[6], vect_train_acc[6], label='train conf 7')
-    plt.plot(vet_rounds[7], vect_train_acc[7], label='train conf 8')
-    plt.plot(vet_rounds[8], vect_train_acc[8], label='train conf 9')
+    # plt.plot(vect_train_rounds[0], vect_train_acc[0], label='train 1')
+    # plt.plot(vect_train_rounds[1], vect_train_acc[1], label='train 2')
+    # plt.plot(vect_train_rounds[2], vect_train_acc[2], label='train 3')
+    # plt.plot(vect_train_rounds[3], vect_train_acc[3], label='train 4')
+    # plt.plot(vect_train_rounds[4], vect_train_acc[4], label='train 5')
+    # plt.plot(vect_train_rounds[5], vect_train_acc[5], label='train 6')
+    # plt.plot(vect_train_rounds[6], vect_train_acc[6], label='train 7')
+    # plt.plot(vect_train_rounds[7], vect_train_acc[7], label='train 8')
+    # plt.plot(vect_train_rounds[8], vect_train_acc[8], label='train 9') 
+    # plt.plot(vect_train_rounds[9], vect_train_acc[9], label='train 10') 
+    # plt.plot(vect_train_rounds[10], vect_train_acc[10], label='train 11') 
+    # plt.plot(vect_train_rounds[11], vect_train_acc[11], label='train 12') 
+    # plt.plot(vect_train_rounds[12], vect_train_acc[12], label='train 13') 
+    # plt.plot(vect_train_rounds[13], vect_train_acc[13], label='train 14') 
+    # plt.plot(vect_train_rounds[14], vect_train_acc[14], label='train 15') 
+    # plt.plot(vect_train_rounds[15], vect_train_acc[15], label='train 16') 
+    # plt.plot(vect_train_rounds[16], vect_train_acc[16], label='train 17') 
+    # plt.plot(vect_train_rounds[17], vect_train_acc[17], label='train 18') 
+    # plt.plot(vect_train_rounds[18], vect_train_acc[18], label='train 19') 
 
-    plt.plot(vet_rounds[9], vect_train_acc[9], label='train conf 10')
-    plt.plot(vet_rounds[10], vect_train_acc[10], label='train conf 11')
-    plt.plot(vet_rounds[11], vect_train_acc[11], label='train conf 12')
-    plt.plot(vet_rounds[12], vect_train_acc[12], label='train conf 13')
-    plt.plot(vet_rounds[13], vect_train_acc[13], label='train conf 14')
-    plt.plot(vet_rounds[14], vect_train_acc[14], label='train conf 15')
-    plt.plot(vet_rounds[15], vect_train_acc[15], label='train conf 16')
-    plt.plot(vet_rounds[16], vect_train_acc[16], label='train conf 17')
-    plt.plot(vet_rounds[17], vect_train_acc[17], label='train conf 18')
-
+    
     # Test
-    plt.plot(vet_rounds[0], vect_test_acc[0], label='test conf 1')
-    plt.plot(vet_rounds[1], vect_test_acc[1], label='test conf 2')
-    plt.plot(vet_rounds[2], vect_test_acc[2], label='test conf 3')
-    plt.plot(vet_rounds[3], vect_test_acc[3], label='test conf 4')
-    plt.plot(vet_rounds[4], vect_test_acc[4], label='test conf 5')
-    plt.plot(vet_rounds[5], vect_test_acc[5], label='test conf 6')
-    plt.plot(vet_rounds[6], vect_test_acc[6], label='test conf 7')
-    plt.plot(vet_rounds[7], vect_test_acc[7], label='test conf 8')
-    plt.plot(vet_rounds[8], vect_test_acc[8], label='test conf 9')
-
-    plt.plot(vet_rounds[9], vect_test_acc[9], label='test conf 10')
-    plt.plot(vet_rounds[10], vect_test_acc[10], label='test conf 11')
-    plt.plot(vet_rounds[11], vect_test_acc[11], label='test conf 12')
-    plt.plot(vet_rounds[12], vect_test_acc[12], label='test conf 13')
-    plt.plot(vet_rounds[13], vect_test_acc[13], label='test conf 14')
-    plt.plot(vet_rounds[14], vect_test_acc[14], label='test conf 15')
-    plt.plot(vet_rounds[15], vect_test_acc[15], label='test conf 16')
-    plt.plot(vet_rounds[16], vect_test_acc[16], label='test conf 17')
-    plt.plot(vet_rounds[17], vect_test_acc[17], label='test conf 18')
-
+    plt.plot(vect_test_rounds[0], vect_test_acc[0], label='test 1')
+    plt.plot(vect_test_rounds[1], vect_test_acc[1], label='test 2')
+    plt.plot(vect_test_rounds[2], vect_test_acc[2], label='test 3')
+    plt.plot(vect_test_rounds[3], vect_test_acc[3], label='test 4')
+    plt.plot(vect_test_rounds[4], vect_test_acc[4], label='test 5')
+    plt.plot(vect_test_rounds[5], vect_test_acc[5], label='test 6')
+    plt.plot(vect_test_rounds[6], vect_test_acc[6], label='test 7')
+    plt.plot(vect_test_rounds[7], vect_test_acc[7], label='test 8')
+    plt.plot(vect_test_rounds[8], vect_test_acc[8], label='test 9')
+    plt.plot(vect_test_rounds[9], vect_test_acc[9], label='test 10') 
+    plt.plot(vect_test_rounds[10], vect_test_acc[10], label='test 11') 
+    plt.plot(vect_test_rounds[11], vect_test_acc[11], label='test 12') 
+    plt.plot(vect_test_rounds[12], vect_test_acc[12], label='test 13') 
+    plt.plot(vect_test_rounds[13], vect_test_acc[13], label='test 14') 
+    plt.plot(vect_test_rounds[14], vect_test_acc[14], label='test 15') 
+    plt.plot(vect_test_rounds[15], vect_test_acc[15], label='test 16') 
+    plt.plot(vect_test_rounds[16], vect_test_acc[16], label='test 17') 
+    plt.plot(vect_test_rounds[17], vect_test_acc[17], label='test 18') 
+    plt.plot(vect_test_rounds[18], vect_test_acc[18], label='test 19') 
+    
     plt.legend(loc='lower right')
     plt.show()
 
