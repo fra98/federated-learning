c4cc5efa947e0466c1ae82e026b554b7a63abd2b
Giorgio Voto <s288179@studenti.polito.it>
2022-02-10 00:52:28 +0100

    Update main_yaml_fedOPT.py

--- main_yaml_fedOPT.py
+++ main_yaml_fedOPT.py
@@ -6,7 +6,7 @@ from src.logger_manager import LoggerHandler
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-CONFIG = "config_light"
+CONFIG = "config_80"
 
 LOG_PATH = ""
 LOG_NAME = f"{CONFIG}.log"
